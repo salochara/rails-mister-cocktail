0df763f0f6789c449f57df09c8bcc32a7078bd86
Salo Charabati <salochara@gmail.com>
2018-11-14 15:02:05 +0200

    Progress

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,43 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
